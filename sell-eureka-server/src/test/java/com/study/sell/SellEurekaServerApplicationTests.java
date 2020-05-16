d3b6882f9248a2555487d91f219a692c05a5948c
zhanghongbing <zhanghongbing@365trade.com.cn>
2020-05-16 19:12:42 +0800

    Initial commit

---
+++ sell-eureka-server/src/test/java/com/study/sell/SellEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ sell-product/src/test/java/com/study/sellproduct/SellProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sellproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
