adc5c112ffeafc1bbca0b06a108129bee3c3f6dd
zhanghongbing <zhanghongbing@365trade.com.cn>
2020-05-28 23:39:01 +0800

    提交配置中心代码

---
+++ sell-config/src/test/java/com/study/sell/config/SellConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sell.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
