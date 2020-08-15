6910a2ab27bc67db9e5453e01db4d03e92f9fbb0
zhanghongbing <zhanghongbing@365trade.com.cn>
2020-08-15 16:35:44 +0800

    提交config配置中心加密配置

--- sell-eureka-server/src/main/java/com/study/sell/config/WebSecurityConfig.java
+++ sell-eureka-server/src/main/java/com/study/sell/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /*Spring Cloud 2.0 以上的security默认启用了csrf检验，要在eurekaServer端配置security的csrf检验为false*/
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
