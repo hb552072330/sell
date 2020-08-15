1ad8f0650c4bb16251ce40773a6e9061d9be40fb
zhanghongbing <zhanghongbing@365trade.com.cn>
2020-08-15 16:13:46 +0800

    提交Eureka注册中心增加权限校验

---
+++ sell-eureka-server/src/main/java/com/study/sell/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.study.sell.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: zhanghongbing
+ * @Description:
+ * @Date: Create in 16:05 2020/8/15
+ * @Modified By:
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
